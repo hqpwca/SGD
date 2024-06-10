24118a2c2ec6f79cebfe0e33253b6821bd8a6499
陈科 <kechen@chenkedeMacBook-Air.local>
2024-06-10 15:46:28 -0400

    SF start

---
+++ layers/SF_projector.cu
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+#include <random>
+
+#include "linear.hh"
+#include "../utils/exception.hh"
+
+__global__ void SF_project(float *proj, float *vol, int3 n3xyz, float3 d3xyz, float *pm, ) {
+    //Calculate Projection points on detector plane for voxel
+    
+    
+    //Normalize and calculate upon projection
+
+}
+
+__global__ void SF_backproject() {
+
+}
+
+void generate_projection_matrix() {
+
+}
+
+void project() {
+
+}
+
+void back_project() {
+    
+}
